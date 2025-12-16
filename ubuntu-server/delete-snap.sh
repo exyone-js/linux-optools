39d59cd4d840208d4a7a36e3799154f3c0118df1
exyone-js <exyone.dev@icloud.com>
2025-12-16 22:34:29 +0800

    Update delete-snap.sh

--- ubuntu-server/delete-snap.sh
+++ ubuntu-server/delete-snap.sh
@@ -1,12 +1,25 @@
+# 查看已安装的snap应用
 snap list
+
+# 卸载所有snap应用
 sudo snap remove --purge $(snap list | awk 'NR>1 {print $1}')
+
+# 停止snap服务
 sudo systemctl stop snapd snapd.socket
+
+# 禁用snap服务
 sudo systemctl disable snapd snapd.socket
+
+# 卸载snapd
 sudo apt purge snapd -y
+
+# 清理snap目录
 sudo rm -rf /var/cache/snapd/
 sudo rm -rf ~/snap
 sudo rm -rf /snap
 sudo rm -rf /var/snap
+
+# 阻止snap重新安装
 sudo tee /etc/apt/preferences.d/nosnap.pref <<EOF
 Package: snapd
 Pin: release a=*
